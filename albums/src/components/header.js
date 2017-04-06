6ec61ccb922821bfc8322573f39d3735257b8c31
FridaSjoholm <frida@fridaesign.se>
2017-04-06 15:20:05 -0700

    Creates Header component

---
+++ albums/src/components/header.js
@@ -0,0 +1,15 @@
+//Import libraries
+import React from 'react';
+import { Text }from 'react-native';
+//Make Component
+
+const Header = () => {
+  return (
+    <Text>
+      Albums
+    </Text>
+  );
+}
+//Make Component available
+
+export default Header;
