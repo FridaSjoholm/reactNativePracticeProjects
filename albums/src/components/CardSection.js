d2b93332f3b8686865e888c44f99a79e5219159c
FridaSjoholm <frida@fridaesign.se>
2017-04-06 19:18:18 -0700

    Styles and renders Card Section

--- albums/src/components/CardSection.js
+++ albums/src/components/CardSection.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const CardSection = () => {
+const CardSection = (props) => {
   return (
-    <View></View>
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
   );
 };
-
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    position: 'relative'
+  }
+};
 export default CardSection;
