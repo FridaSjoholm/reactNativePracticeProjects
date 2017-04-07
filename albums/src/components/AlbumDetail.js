9f6bc46dea1826b6bad7552aa28f9fcb6d565c43
FridaSjoholm <frida@fridaesign.se>
2017-04-06 21:50:56 -0700

    Links Button to amazon store for album

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-import Button from './Button'
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image,
+    url
+   } = album;
   const {
     thumbnailStyle,
     thumbnailContainerStyle,
@@ -36,7 +42,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => console.log(title)} />
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
