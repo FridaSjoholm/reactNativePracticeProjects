0bcf731e0b11bd0f519de70cc0f3c59e74223adf
FridaSjoholm <frida@fridaesign.se>
2017-04-07 17:19:35 -0700

    Sets up firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+    apiKey: 'AIzaSyC5Dmm2Yt5SrdBY0GG-5izNkiyQoBZz5FY',
+    authDomain: 'auth-b10bf.firebaseapp.com',
+    databaseURL: 'https://auth-b10bf.firebaseio.com',
+    projectId: 'auth-b10bf',
+    storageBucket: 'auth-b10bf.appspot.com',
+    messagingSenderId: '900281358336'
+    });
+  }
+
   render(){
     return (
       <View>
