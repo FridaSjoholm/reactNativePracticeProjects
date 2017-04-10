3517ace5b7e529d94747f0e49bfbece82176c54c
FridaSjoholm <frida@fridaesign.se>
2017-04-10 09:30:27 -0700

    Adds TextInput to LoginForm and eslint config

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
-import LoginForm from './components/LoginForm'
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  componentWillMount(){
+  componentWillMount() {
     firebase.initializeApp({
     apiKey: 'AIzaSyC5Dmm2Yt5SrdBY0GG-5izNkiyQoBZz5FY',
     authDomain: 'auth-b10bf.firebaseapp.com',
@@ -16,7 +16,7 @@ class App extends Component {
     });
   }
 
-  render(){
+  render() {
     return (
       <View>
         <Header headerText="Authentication" />
