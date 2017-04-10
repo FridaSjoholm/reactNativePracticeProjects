b36070d6c80134e57adbfa123d8169aa5f0b41c3
FridaSjoholm <frida@fridaesign.se>
2017-04-10 09:06:32 -0700

    Adds LoginForm Component

---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import {}
+
+class LoginForm extends Component {
+  render(){
+    return(
+      <View />
+    );
+  }
+}
+
+export default LoginForm;
