f3b1df983658d975535a56a82105238cb3bfcace
FridaSjoholm <frida@fridaesign.se>
2017-04-10 09:11:51 -0700

    Adds basic content and displays loginform in App

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import {}
+import { Button, Card, CardSection } from './common'
 
 class LoginForm extends Component {
   render(){
     return(
-      <View />
+      <Card>
+        <CardSection />
+        <CardSection />
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
