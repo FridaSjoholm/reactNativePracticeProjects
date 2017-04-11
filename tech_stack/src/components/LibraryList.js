8a8f757effa4558028939a1e826b339acbdbf54d
FridaSjoholm <frida@fridaesign.se>
2017-04-11 11:15:27 -0700

    Updates LibraryList with datasource

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.DataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(){
+    
+  }
+
   render() {
-    console.log(this.props);
-    return null;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
