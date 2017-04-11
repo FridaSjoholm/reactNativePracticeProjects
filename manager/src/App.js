95c6801fb6eea0e437279f90133961b11e2edc7b
FridaSjoholm <frida@fridaesign.se>
2017-04-11 15:54:47 -0700

    Sets up firebase in application

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAxE4agwyNtT0oPCy9QzCoh9JhqHe_L064',
+      authDomain: 'manager-9a613.firebaseapp.com',
+      databaseURL: 'https://manager-9a613.firebaseio.com',
+      projectId: 'manager-9a613',
+      storageBucket: 'manager-9a613.appspot.com',
+      messagingSenderId: '138385007120'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
