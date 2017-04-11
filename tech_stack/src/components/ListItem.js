01cb4d6c1d788ecb29ec35f028a86d7c97eaa091
FridaSjoholm <frida@fridaesign.se>
2017-04-11 14:52:49 -0700

    Updates readme with demonstrating gif and fixes typo

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -11,7 +11,7 @@ import * as actions from '../actions';
 
 class ListItem extends Component {
   componentWillUpdate() {
-    LayoutAnimation.linear();
+    LayoutAnimation.spring();
   }
   renderDescription() {
     const { library, expanded } = this.props;
