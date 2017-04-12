c1d18952606cbc62ecb04fad42390815d1c3d713
FridaSjoholm <frida@fridaesign.se>
2017-04-12 09:56:45 -0700

    Creates fiirst action creator

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -3,12 +3,17 @@ import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   render() {
+    onEmailChange(text){
+
+    }
+
     return (
       <Card>
         <CardSection>
           <Input
             label="Email"
             placeholder="name@mail.com"
+            onChangeText={this.onEmailChange.bind(this)}
           />
         </CardSection>
 
