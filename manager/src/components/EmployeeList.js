8e691b804de5dec0bdc28ac45aa7576b4e22e0f7
FridaSjoholm <frida@fridaesign.se>
2017-04-13 09:20:19 -0700

    Adds employeelist component and adds it as scene

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Employee A </Text>
+        <Text> Employee B </Text>
+        <Text> Employee C </Text>
+        <Text> Employee D </Text>
+        <Text> Employee E </Text>
+        <Text> Employee F </Text>
+        <Text> Employee G </Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
